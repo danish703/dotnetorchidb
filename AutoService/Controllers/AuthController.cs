846dba6827269faf4a94b9560ecded57e72b9e9e
danish703 <dipu.danish@outlook.com>
2022-06-13 18:04:12 +0545

    initial

---
+++ AutoService/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AutoService.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ViewResult Signup()
+        {
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        public ViewResult Dashboard()
+        {
+            return View();
+        }
+
+        public ViewResult AddVehicle()
+        {
+            return View();
+        }
+
+        public ViewResult AddService()
+        {
+            return View();
+        }
+    }
+}
